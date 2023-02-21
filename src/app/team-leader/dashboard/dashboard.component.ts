cf31fd6f7fce00e6a06ec375f23de182dc52698b
Maladroy <hoangchilam@protonmail.com>
2023-02-21 12:41:46 +0700

    feat: team leader can now view projects

--- src/app/team-leader/dashboard/dashboard.component.ts
+++ src/app/team-leader/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/services/auth.service';
-
+import { ProjectService } from 'src/services/project.service';
+import jwt_decode from 'jwt-decode';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,14 +10,29 @@ import { AuthService } from 'src/services/auth.service';
 })
 export class DashboardComponent implements OnInit {
   role: string;
+  username: string;
+  project: any;
 
   constructor(
     private authService: AuthService,
+    private projectService: ProjectService
   ) {
 
   }
 
   ngOnInit() {
+    const user = jwt_decode(this.authService.getToken()) as {
+      role: string,
+      id: string,
+      username: string
+    }
+
     this.role = this.authService.getAuthenticatedUserRole() as string
+    this.username = user.username
+    this.projectService.getUserProjects(user.id).subscribe(res => {
+      console.log(res);
+    })
+
+    console.log(this.project);
   }
 }
